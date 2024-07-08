d2b3ec764a1541de61d617d42c97235e9dd98d92
kanayaya <kanayaya@mail.ru>
2024-07-09 07:23:02 +0800

    add javadoc

--- src/main/java/com/kanayaya/XLSParse/InnerClassImplementation/TransitiveBiFunction.java
+++ src/main/java/com/kanayaya/XLSParse/InnerClassImplementation/TransitiveBiFunction.java
@@ -4,10 +4,21 @@ import lombok.NonNull;
 
 import java.util.function.BiFunction;
 
-
+/**
+ * Транзитивная функция, передающая первый параметр по всей цепочке, <br>
+ * второй параметр передаваемый в цепочку является результатом её выполнения
+ * @param <T> Транзитивно передаваемый в цепочке тип
+ * @param <C> Тип, меняющийся по цепочке
+ * @param <R> Тип результата выполнения функции
+ */
 @FunctionalInterface
 public interface TransitiveBiFunction<T, C, R> extends BiFunction<T, C, R> {
-    default <R2> TransitiveBiFunction<T, C, R2> andThen(@NonNull BiFunction<? super T, ? super R, R2> that) {
-        return (transitive, chained) -> that.apply(transitive, this.apply(transitive, chained));
+    /**
+     * @param that Транзитивная функция, которая выполнится следующей
+     * @param <R2> Тип результата, возвращаемого следующей функцией
+     * @return Спаренные в цепочку функции
+     */
+    default <R2> TransitiveBiFunction<T, C, R2> andThen(@NonNull TransitiveBiFunction<? super T, ? super R, R2> that) {
+        return (transitive, chained) -> that.apply(transitive, apply(transitive, chained));
     }
 }
