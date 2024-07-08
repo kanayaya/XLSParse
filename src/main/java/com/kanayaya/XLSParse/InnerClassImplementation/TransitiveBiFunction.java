9bb3c70b947fded403c43b76fdb1771f9f923b3b
kanayaya <kanayaya@mail.ru>
2024-07-09 05:12:29 +0800

    fix tbf to use less specific interface

--- src/main/java/com/kanayaya/XLSParse/InnerClassImplementation/TransitiveBiFunction.java
+++ src/main/java/com/kanayaya/XLSParse/InnerClassImplementation/TransitiveBiFunction.java
@@ -7,7 +7,7 @@ import java.util.function.BiFunction;
 
 @FunctionalInterface
 public interface TransitiveBiFunction<T, C, R> extends BiFunction<T, C, R> {
-    default <R2> TransitiveBiFunction<T, C, R2> andThen(@NonNull TransitiveBiFunction<? super T, ? super R, R2> that) {
-        return (transitive, chained) -> that.apply(transitive, apply(transitive, chained));
+    default <R2> TransitiveBiFunction<T, C, R2> andThen(@NonNull BiFunction<? super T, ? super R, R2> that) {
+        return (transitive, chained) -> that.apply(transitive, this.apply(transitive, chained));
     }
 }
