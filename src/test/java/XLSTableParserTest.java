c8fd1c201e9de5152e0acf5aa779fa7c9b9f0fcd
kanayaya <kanayaya@mail.ru>
2024-07-11 21:55:30 +0800

    add test cases for coverage

--- src/test/java/XLSTableParserTest.java
+++ src/test/java/XLSTableParserTest.java
@@ -1,6 +1,4 @@
-
 import com.kanayaya.XLSParse.InnerClassImplementation.XLSTableParser;
-import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 
@@ -27,13 +25,54 @@ class XLSTableParserTest {
                 .thenPutInto(System.out::println)
 
                 .thenContinueSameSheet()
-                .findRowThat(row -> new DataFormatter().formatCellValue(row.getCell(row.getFirstCellNum())).contains("title 1"))
+                .findRowWhereCell(0).isNotNull().and().isNotEmpty().and().stringValueContains("title 1")
                 .thenSkip(1)
                 .endIfCell(2).isNull().or().isEmpty()
                 .getEntityFrom(() -> new LinkedHashMap<String, String>())
 
                 .thenForNextColumn((dto, cell) -> dto.put(cell.getStringCellValue(), cell.getStringCellValue()))
                 .thenForNextColumn((dto, cell) -> dto.put(cell.getRawValue(), cell.getStringCellValue()))
+                .thenPutInto(System.out::println)
+
+                .parse(book);
+
+        XLSTableParser.fromSheet(0)
+                .findRowThat(row -> row.getCell(row.getFirstCellNum()).getStringCellValue().contains("title 1"))
+                .thenSkip(1)
+                .endIfCell(0).isNull().or().isEmpty().or().isNotNumeric()
+                .getEntityFrom(() -> new LinkedHashMap<String, String>())
+
+                .thenForNextColumnStringified((dto, s) -> dto.put(s, s))
+                .thenForNextColumn((dto, cell) -> dto.put(cell.getRawValue(), Integer.toString(Double.valueOf(cell.getNumericCellValue()).intValue())))
+                .thenPutInto(System.out::println)
+
+                .thenRestartSameSheet()
+                .findRowWhereCell(0).isNotNull().and().isNotEmpty().and().stringValueContains("title 1")
+                .thenSkip(1)
+                .endIfCell(2).isNull().or().isEmpty()
+                .getEntityFrom(() -> new LinkedHashMap<String, String>())
+
+                .thenForColumn(0, (dto, cell) -> dto.put(Double.toString(cell.getNumericCellValue()), Double.toString(cell.getNumericCellValue())))
+                .thenForColumn(1, (dto, cell) -> dto.put(cell.getRawValue(), Double.toString(cell.getNumericCellValue())))
+                .thenPutInto(System.out::println)
+
+                .thenFromSheet(0)
+                .findRowWhereCell(0).isNotNull().and().isNotEmpty().and().stringValueContains("title 1")
+                .thenSkip(1)
+                .endIfCell(2).isNull().or().isEmpty()
+                .getEntityFrom(() -> new LinkedHashMap<String, String>())
+
+                .thenForColumn(0, (dto, cell) -> dto.put(Double.toString(cell.getNumericCellValue()), Double.toString(cell.getNumericCellValue())))
+                .thenForColumn(1, (dto, cell) -> dto.put(cell.getRawValue(), Double.toString(cell.getNumericCellValue())))
+                .thenPutInto(System.out::println)
+
+                .thenFromSheet(book.getSheetName(0))
+                .findRowWhereCell(0).isNotNull().and().isNotEmpty().and().stringValueContains("title 1")
+                .noSkip()
+                .endIfCell(0).isNull().or().isEmpty().or().isNotString().orOtherCell(1).isNotNull()
+                .getEntityFrom(() -> new LinkedHashMap<String, String>())
+
+                .thenForColumn(0, (dto, cell) -> dto.put(cell.getStringCellValue(), cell.getStringCellValue()))
                 .thenPutInto(System.out::println)
 
                 .parse(book);
